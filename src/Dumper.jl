65d5a4010ca131d123de986fd5913125587bd144
Alex Wietek <awietek@pks.mpg.de>
2023-08-07 13:26:31 +0200

    First commit

--- src/Dumper.jl
+++ src/Dumper.jl
@@ -1,5 +1,18 @@
 module Dumper
 
-# Write your package code here.
+using HDF5
+using Printf
+
+export DumpFile, dump_open
+
+struct DumpFile
+    filename::String
+    fid::HDF5.File
+end
+
+function dump_open(filename::String)
+    fid = h5open(filename, "cw")
+    return DumpFile(filename, fid)    
+end
 
 end
